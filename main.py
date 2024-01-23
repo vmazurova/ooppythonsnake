2c512a18997607079aec0a1b77baf12989076187
Viki Mazurova <98098663+vmazurova@users.noreply.github.com>
2024-01-23 08:17:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ class Snake():
 
 class Food():
     def __init__(self):
-        x = random.randint(0, int((GAME_WIDTH / SPACE_SIZE))-1) * SPACE_SIZE
-        y = random.randint(0, int((GAME_HEIGHT / SPACE_SIZE)-1)) * SPACE_SIZE
+        x = random.randint(0, int((GAME_WIDTH / SPACE_SIZE)-2)) * SPACE_SIZE
+        y = random.randint(0, int((GAME_HEIGHT / SPACE_SIZE)-2)) * SPACE_SIZE
         self.coordinates = [x, y]
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
         #nahodne generovani jidla
